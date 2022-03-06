3fce90dd0f5684711176efa5f076fb62a6652074
Simone Paolucci <magicianred@gmail.com>
2022-03-06 09:19:32 +0100

    Initialize feature/add_integration_test_services

---
+++ it.example.dotnetcore5/it.example.dotnetcore5.dal.json/Repositories/PostsRepository.cs
@@ -0,0 +1,79 @@
+﻿using it.example.dotnetcore5.dal.json.Models;
+using it.example.dotnetcore5.domain.Interfaces.Models;
+using it.example.dotnetcore5.domain.Interfaces.Repositories;
+using it.example.dotnetcore5.domain.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace it.example.dotnetcore5.dal.json.Repositories
+{
+    /// <summary>
+    /// Repository of posts
+    /// </summary>
+    public class PostsRepository : IPostsRepository
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public PostsRepository()
+        {
+            PostsRepository.LoadData();
+        }
+
+        /// <summary>
+        /// Retrieve all Posts items
+        /// </summary>
+        /// <returns>list of post</returns>
+        public IEnumerable<IPost> GetAll()
+        {
+            IEnumerable<IPost> posts = MemoryCache<Post>.Items;
+
+            return posts;
+        }
+
+        /// <summary>
+        /// Retrieve post by own id
+        /// </summary>
+        /// <param name="id">id of the post to retrieve</param>
+        /// <returns>the post, null if id not found</returns>
+        public IPost GetById(int id)
+        {
+            IPost post = MemoryCache<Post>.Items.Find(item => item.Id == id);
+
+            return post;
+        }
+
+        public IPost AddPost(IPost item)
+        {
+            Post newEntry = (Post)item;
+            newEntry.Id = MemoryCache<Post>.Items.Max(item => item.Id) + 1;
+            MemoryCache<Post>.Items.Add(newEntry);
+
+            // TO DO save items into json file
+
+            return this.GetById(newEntry.Id);
+        }
+
+        public void RemoveAll()
+        {
+            MemoryCache<Post>.Items.RemoveAll(x => x.Id > -1);
+        }
+
+
+        #region private methods
+
+        private static List<IPost> LoadData()
+        {
+            var fileContent = System.IO.File.ReadAllText("Resources/posts.json");
+            var posts = JsonConvert.DeserializeObject<List<Post>>(fileContent);
+            if(posts != null)
+            {
+                return posts.ToList<IPost>();
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ it.example.dotnetcore5/it.example.dotnetcore5.domain/Interfaces/Repositories/IPostsRepository.cs
@@ -0,0 +1,13 @@
+﻿using it.example.dotnetcore5.domain.Interfaces.Models;
+using System.Collections.Generic;
+
+namespace it.example.dotnetcore5.domain.Interfaces.Repositories
+{
+    public interface IPostsRepository
+    {
+        IEnumerable<IPost> GetAll();
+        IPost GetById(int id);
+        IPost AddPost(IPost item);
+        void RemoveAll();
+    }
+}
